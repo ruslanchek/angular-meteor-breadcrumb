47f016d448d4162d3b631ecc748531c22e354ef8
Ruslan Shashkov <ruslan.shashkov@ibitcy.com>
2015-09-10 11:46:53 +0300

    Summary

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 var packageName = 'rshashkov:angular-meteor-breadcrumb';
 var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
 var version = '0.0.1';
-var summary = 'Angular-breadcrumb is a module for AngularJS, which generates a breadcrumb for any page of your application. It is strongly based on the ui-router framework and its hierarchical tree of states.';
+var summary = 'Angular-breadcrumb is a module for AngularJS, which generates a breadcrumb for any page of your application. Based on the ui-router.';
 var gitLink = 'https://github.com/ruslanchek/angular-meteor-breadcrumb';
 var documentationFile = 'README.md';
 
